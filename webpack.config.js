01a2c978d8da230da05fb0e103867debb5ad44d3
lawlerblader <jacob@hamblin.cc>
2016-02-28 17:41:25 -0700

    my way

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-var config = {
+module.exports = {
   devtool : 'eval-source-map',
 
   entry: [
@@ -23,15 +25,22 @@ var config = {
         test: /\.js$/, loader: 'babel', exclude: /node_modules/,
         query: { presets: ['es2015', 'react', 'stage-0'] }
       },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
-      },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.css$/, loader: 'css-loader!postcss-loader' },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!postcss-loader!sass') },
+      { test: /\.png$/, loader: 'url-loader?prefix=img/&limit=5000' },
+      { test: /\.jpg$/, loader: 'url-loader?prefix=img/&limit=5000' },
+      { test: /\.gif$/, loader: 'url-loader?prefix=img/&limit=5000' },
+      { test: /\.woff$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.woff2$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.eot$/, loader: 'file-loader?prefix=font/' },
+      { test: /\.ttf$/, loader: 'file-loader?prefix=font/' },
+      { test: /\.svg$/, loader: 'file-loader?prefix=font/' },
+      { test: /\.txt$/, loader: 'raw-loader' },
     ]
   },
-  sassLoader: {
-    includePaths: [path.join(__dirname, "style")]
-  }
+  plugins: [
+    new ExtractTextPlugin('../css/[name].css'),
+  ],
+  postcss: [ autoprefixer({ browsers: '> 5%'}) ]
 };
-
-module.exports = config;
